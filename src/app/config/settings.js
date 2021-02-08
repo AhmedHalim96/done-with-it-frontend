7a64071fb3c5948e74bb143bc57b6dfd31ea6228
Ahmed Halim <ahmedhalim96@protonmail.com>
2021-02-08 08:18:10 +0200

    changing dev url

--- src/app/config/settings.js
+++ src/app/config/settings.js
@@ -1,7 +1,7 @@
 const settings = {
 	dev: {
-		apiUrl: "http://www.donewithit.com/api",
-		assetsUrl: "http://www.donewithit.com/storage/assets",
+		apiUrl: "http://127.0.0.1:8000/api",
+		assetsUrl: "http://127.0.0.1:8000/storage/assets",
 	},
 };
 
