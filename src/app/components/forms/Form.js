5d0fe318e0f9f8992be00b5defe237ed3fd453ab
Ahmed Halim <ahmedhalim96@protonmail.com>
2021-02-11 03:10:36 +0200

    adding className parameter to  Form

--- src/app/components/forms/Form.js
+++ src/app/components/forms/Form.js
@@ -1,14 +1,20 @@
 import { Formik, Form } from "formik";
 import React from "react";
 
-const AppForm = ({ initialValues, onSubmit, valdiationSchema, children }) => {
+const AppForm = ({
+	initialValues,
+	onSubmit,
+	valdiationSchema,
+	className = "",
+	children,
+}) => {
 	return (
 		<Formik
 			initialValues={initialValues}
 			onSubmit={onSubmit}
 			validationSchema={valdiationSchema}
 		>
-			{() => <Form className="form">{children}</Form>}
+			{() => <Form className={"form " + className}>{children}</Form>}
 		</Formik>
 	);
 };
