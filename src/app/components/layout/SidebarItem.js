f229039cfaa1f8eb01ec776049788ff9c1d8ef4a
Ahmed Halim <ahmedhalim96@protonmail.com>
2021-02-19 08:07:59 +0200

    closing sidebar when a link is clicked

--- src/app/components/layout/SidebarItem.js
+++ src/app/components/layout/SidebarItem.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import LayoutContext from "../../layout/context";
 
 const SidebarItem = ({ to, title }) => {
+	const { closeSidebar } = useContext(LayoutContext);
 	return (
 		<li className="sidebar__item">
-			<Link to={to} className="sidebar__link">
+			<Link to={to} className="sidebar__link" onClick={closeSidebar}>
 				{title}
 			</Link>
 		</li>
