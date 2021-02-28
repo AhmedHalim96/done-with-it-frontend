70d95c0b9cb6b1a54640edc31e5ef1eb5c0196fc
Ahmed Halim <ahmedhalim96@protonmail.com>
2021-02-28 14:55:12 +0200

    fixing a small mistake

--- src/app/navigation/routes.js
+++ src/app/navigation/routes.js
@@ -2,7 +2,7 @@ export default Object.freeze({
 	CREATE_LISTING: "/listing/create",
 	EDIT_DETAILS: "/listing/:listingId/update",
 	FEED: "/feed",
-	LISTING_DETAILS: "/listing/:listingId",
+	LISTING_DETAILS: "/listings/:listingId",
 	LOGIN: "/login",
 	REGISTER: "/register",
 	WELCOME: "/welcome",
