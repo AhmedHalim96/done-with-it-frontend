f98d41ae28561dba57544a2b1536f983b42ca329
Ahmed Halim <ahmedhalim96@protonmail.com>
2021-03-29 03:31:48 +0200

    fixing no scrolling bug

--- src/App.js
+++ src/App.js
@@ -11,15 +11,18 @@ import AuthContext from "./app/auth/context";
 
 const App = () => {
 	const [sidebarState, setSidebarState] = useState(false);
+	const [scrolling, setScrolling] = useState(true);
 	const [authenticated, setAuthenticated] = useState(true);
 
 	return (
-		<div className="app u-no-scrolling">
+		<div className={`app ${!scrolling ? "u-no-scrolling" : ""}`}>
 			<LayoutContext.Provider
 				value={{
 					isSidebarOpen: sidebarState,
 					openSidebar: () => setSidebarState(true),
 					closeSidebar: () => setSidebarState(false),
+					scrolling,
+					setScrolling,
 				}}
 			>
 				<AuthContext.Provider
--- src/app/components/layout/Backdrop.js
+++ src/app/components/layout/Backdrop.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import LayoutContext from "../../layout/context";
 
 const Backdrop = ({ close }) => {
-	return <div className="backdrop" onClick={close} />;
+	const { setScrolling } = useContext(LayoutContext);
+	useEffect(() => {
+		setScrolling(false);
+	}, []);
+
+	useEffect(() => () => setScrolling(true), []);
+	return (
+		<div
+			className="backdrop"
+			onClick={() => {
+				setScrolling(true);
+				close();
+			}}
+		/>
+	);
 };
 
 export default Backdrop;
