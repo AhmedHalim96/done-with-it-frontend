494d85ae2b23e2fb7e9099ca3a3da545770409f5
Ahmed Halim <ahmedhalim96@protonmail.com>
2021-03-30 08:27:28 +0200

    refactoring handleClose

--- src/app/components/layout/Backdrop.js
+++ src/app/components/layout/Backdrop.js
@@ -3,20 +3,20 @@ import LayoutContext from "../../layout/context";
 
 const Backdrop = ({ close }) => {
 	const { setScrolling } = useContext(LayoutContext);
+
 	useEffect(() => {
+		// disable scrolling when backdrop is mounted
 		setScrolling(false);
 	}, []);
 
+	// re-enable scrolling when backdrop is unmounted
 	useEffect(() => () => setScrolling(true), []);
-	return (
-		<div
-			className="backdrop"
-			onClick={() => {
-				setScrolling(true);
-				close();
-			}}
-		/>
-	);
+
+	const handleClose = () => {
+		setScrolling(true);
+		close();
+	};
+	return <div className="backdrop" onClick={handleClose} />;
 };
 
 export default Backdrop;
