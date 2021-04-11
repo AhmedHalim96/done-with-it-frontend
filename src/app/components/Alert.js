83f1f3d38907cb52f1d71a116ad81a1c5de46e3c
Ahmed Halim <ahmedhalim96@protonmail.com>
2021-04-11 12:58:38 +0200

    removing padding box from alert

--- src/app/components/Alert.js
+++ src/app/components/Alert.js
@@ -2,12 +2,9 @@ import React from "react";
 
 function Alert({ message }) {
 	return (
-		<>
-			<div className="u-pt-1" />
-			<div className="alert">
-				<p className="alert_message">{message}</p>
-			</div>
-		</>
+		<div className="alert">
+			<p className="alert_message">{message}</p>
+		</div>
 	);
 }
 
