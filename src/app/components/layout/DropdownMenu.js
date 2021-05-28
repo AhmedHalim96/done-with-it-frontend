cfb8ee192ff699fd465d77ec26b931e3d303c515
Ahmed Halim <ahmedhalim96@protonmail.com>
2021-05-28 09:54:26 +0200

    creating & styling a dropdown menu

---
+++ src/app/components/layout/DropdownMenu.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const DropdownMenu = ({ className = "", header, items }) => {
+	const [opened, setOpened] = React.useState(false);
+
+	return (
+		<div className={"dropdown " + className}>
+			<h3 className="dropdown__button" onClick={_ => setOpened(!opened)}>
+				{header}
+				<span
+					className={`dropdown__indicator dropdown__indicator-${
+						opened ? "down" : "right"
+					}`}
+				/>
+			</h3>
+			{opened && (
+				<ul className="dropdown__list">
+					{items.map((item, index) => (
+						<li className="dropdown__item">{item}</li>
+					))}
+				</ul>
+			)}
+		</div>
+	);
+};
+
+export default DropdownMenu;
